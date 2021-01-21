a8eff675be35246b1badc26d5a4820bec2c71931
Cholado <sntgmtnz@gmail.com>
2021-01-21 10:52:27 -0500

    can't do it on paper even with the explanation at hand wtf

---
+++ 0x09-csharp-linear_algebra/2-magnitude_3D.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from math import sqrt
+print("Input vector values:")
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
+d = sqrt(a**2 + b**2 + c**2)
+print("The length of the  length of a given vector", "%.2f" % d )
